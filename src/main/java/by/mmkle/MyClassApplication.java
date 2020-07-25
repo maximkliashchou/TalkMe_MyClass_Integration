7a0ece312a8248486fb2d963c0172064dda9b2dc
Максим Клещёв <mmkleschev@gmail.com>
2020-07-26 02:37:20 +0300

    26/07 Kleschev M. add new user every 5 minutes

--- src/main/java/by/mmkle/MyClassApplication.java
+++ src/main/java/by/mmkle/MyClassApplication.java
@@ -1,18 +1,24 @@
 package by.mmkle;
 
 import by.mmkle.bean.User;
+import by.mmkle.service.InitializationService;
 import by.mmkle.service.MyClassService;
 import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableFeignClients
 public class MyClassApplication{
 
-	public static void main(String[] args) throws ParseException {
-		SpringApplication.run(MyClassApplication.class, args);
+	public static void main(String[] args) throws ParseException, IOException, InterruptedException {
+		ApplicationContext ctx = SpringApplication.run(MyClassApplication.class, args);
+		InitializationService service = ctx.getBean(InitializationService.class);
+		service.initialize();
 	}
 
 }
--- src/main/java/by/mmkle/service/InitializationService.java
+++ src/main/java/by/mmkle/service/InitializationService.java
@@ -1,12 +1,16 @@
 package by.mmkle.service;
 
 import by.mmkle.bean.Result;
+import by.mmkle.bean.User;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class InitializationService {
@@ -17,13 +21,25 @@ public class InitializationService {
     @Autowired
     private TalkMeService talkMeService;
 
-    public void initialize() throws IOException, ParseException {
+    public void initialize() throws IOException, ParseException, InterruptedException {
         while(true){
             List<Result> resultList = talkMeService.getAllUsersFromTalkMe();
-            resultList.stream().forEach(el ->{
 
-            });
-
+            System.out.println(resultList);
+            for (int i = 0; i < resultList.size(); i++){
+                LocalDateTime messageTime = resultList.get(i).getTime();
+                LocalDateTime timeNow = LocalDateTime.now();
+                Duration duration = Duration.between(messageTime,timeNow);
+                if (duration.toMinutes() <= 6){
+                    myClassService.createUser(User.builder()
+                            .name(resultList.get(i).getName())
+                            .email(resultList.get(i).getEmail())
+                            .phone(resultList.get(i).getPhone())
+                            .build()
+                    );
+                }
+            }
+            Thread.sleep(300000);
         }
     }
 }
